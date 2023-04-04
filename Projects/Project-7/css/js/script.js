14406221baf4c7b5592cb3d7b9aa68d4926f6508
Animesh Tiwari <swdev.animesh@gmail.com>
2023-04-04 18:57:19 +0530

    1st page done . testing and debugging remaining . rest project after a week

--- Projects/Project-7/css/js/script.js
+++ Projects/Project-7/css/js/script.js
@@ -4,13 +4,20 @@ let showMenu = false;
 console.log(menu_btn);
 menu_btn.addEventListener("click", toggleMenu);
 const navbar = document.getElementById("navbar-nav");
+const body = document.getElementById("full");
+console.log(body);
 function toggleMenu() {
   if (!showMenu) {
     menu_btn.classList.add("open");
+    navbar.classList.remove("trans");
     navbar.classList.add("menu_flex");
+    navbar.classList.add("blur");
     showMenu = true;
   } else {
+    navbar.classList.remove("blur");
     menu_btn.classList.remove("open");
+    navbar.classList.add("trans");
+    navbar.classList.remove("menu_flex");
     showMenu = false;
   }
 }
