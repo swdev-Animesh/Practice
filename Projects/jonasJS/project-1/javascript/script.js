427139e04e47b9ba38659658996deb4aff88ece3
Animesh Tiwari <swdev.animesh@gmail.com>
2023-04-22 01:27:34 +0530

    3 challenges done

--- Projects/jonasJS/project-1/javascript/script.js
+++ Projects/jonasJS/project-1/javascript/script.js
@@ -26,3 +26,16 @@ if (bmi_mark > bmi_john) {
   console.log(`bmi of john is ${bmi_john}`);
 }
 //challenge 2 end
+//challenge 3
+let dolphin_score_avg = (97 + 112 + 101) / 3;
+let koalas_score_avg = (109 + 95 + 106) / 3;
+console.log(dolphin_score_avg, koalas_score_avg);
+if (dolphin_score_avg > koalas_score_avg && dolphin_score_avg >= 100) {
+  console.log(`dolphins won the match : ${dolphin_score_avg}`);
+} else if (dolphin_score_avg === koalas_score_avg && dolphin_score_avg >= 100) {
+  console.log(`Match tied`);
+} else if (dolphin_score_avg < koalas_score_avg && koalas_score_avg >= 100) {
+  console.log(`koalas won the match : ${koalas_score_avg}`);
+}
+
+//challenge 3 end
