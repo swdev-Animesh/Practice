95bda0241ff3619fbcf70e520d2b96a194f9a8cc
Animesh <swdev.animesh@gmail.com>
2023-08-12 14:16:28 +0530

    ok

--- react-comp/react-complete/src/App.js
+++ react-comp/react-complete/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import logo from "./logo.svg";
+import "./App.css";
+import Modal from "./modal-components/Modal";
+import ItemRender from "./components/ItemList/ItemRender";
 function App() {
+  const listData = [
+    {
+      date: new Date(2023, 8, 11),
+      expense: "1. Something that I Bought on that day",
+      amount: 53,
+    },
+    {
+      date: new Date(2023, 3, 12),
+      expense: "2 .Something that I Bought on that day",
+      amount: 52,
+    },
+    {
+      date: new Date(2023, 4, 10),
+      expense: "3 .Something that I Bought on that day",
+      amount: 51,
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Modal></Modal>
+      <hr />
+      <div className="container conatiner-fluid mx-auto text-light text-center">
+        Lets Start This Project!
+      </div>
+      <ItemRender date="DATE" info="Expense Name" amount="Amount"></ItemRender>
+      <ItemRender
+        date={listData[0].date}
+        info={listData[0].expense}
+        amount={listData[0].amount}
+      ></ItemRender>
+      <ItemRender
+        date={listData[1].date}
+        info={listData[1].expense}
+        amount={listData[1].amount}
+      ></ItemRender>
+      <ItemRender
+        date={listData[2].date}
+        info={listData[2].expense}
+        amount={listData[2].amount}
+      ></ItemRender>
     </div>
   );
 }
---
+++ react-comp/react-complete/src/components/AddItem/AddItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AddItem=(props) =>{
+
+
+    return(
+        <div className="container bg"
+    )
+
+}
