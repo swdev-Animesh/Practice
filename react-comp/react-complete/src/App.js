ba36867b07f3b5cf131c27c4ce778156f3744c7f
Animesh <swdev.animesh@gmail.com>
2023-09-07 19:46:33 +0530

    starting anew

--- react-comp/react-complete/src/App.js
+++ react-comp/react-complete/src/App.js
@@ -1,58 +1,5 @@
-//import logo from "./logo.svg";
-import "./App.css";
-import Modal from "./modal-components/Modal";
-import ItemRender from "./components/ItemList/ItemRender";
-import ModalBootstrap from "./modal-components/ModalBootstrap";
-import AddItem from "./components/AddItem/AddItem";
 function App() {
-  const listData = [
-    {
-      date: new Date(2023, 8, 11),
-      expense: "1. Something that I Bought on that day",
-      amount: 53,
-    },
-    {
-      date: new Date(2023, 3, 12),
-      expense: "2 .Something that I Bought on that day",
-      amount: 52,
-    },
-    {
-      date: new Date(2023, 4, 10),
-      expense: "3 .Something that I Bought on that day",
-      amount: 51,
-    },
-  ];
-  // making a comment for seeing git diff option
-  return (
-    <div className="App">
-      {/*modal using bootstrap component*/}
-      <ModalBootstrap></ModalBootstrap>
-      {/*modal using js and react and react logic */}
-      <Modal></Modal>
-
-      <hr />
-      <AddItem></AddItem>
-      <div className="container conatiner-fluid mx-auto text-light text-center">
-        Lets Start This Project!
-      </div>
-      <ItemRender date="DATE" info="Expense Name" amount="Amount"></ItemRender>
-      <ItemRender
-        date={listData[0].date}
-        info={listData[0].expense}
-        amount={listData[0].amount}
-      ></ItemRender>
-      <ItemRender
-        date={listData[1].date}
-        info={listData[1].expense}
-        amount={listData[1].amount}
-      ></ItemRender>
-      <ItemRender
-        date={listData[2].date}
-        info={listData[2].expense}
-        amount={listData[2].amount}
-      ></ItemRender>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
