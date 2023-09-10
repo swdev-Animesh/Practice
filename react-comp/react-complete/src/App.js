5a09b8363c3612966d13cd85d8c43fd65ff33ebf
Animesh <swdev.animesh@gmail.com>
2023-09-10 14:57:33 +0530

    commented unsed components

--- react-comp/react-complete/src/App.js
+++ react-comp/react-complete/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 //importing components for sticky and modal
 // import Sticky from "./Components/Sticky/Sticky";
 // import Modal from "./Components/Modal/Modal";
-import ModalBs from "./Components/Modal/ModalBs";
+//import ModalBs from "./Components/Modal/ModalBs";
 
 function App() {
   //These are states and functions used for making and working of Modal with react logic .
@@ -16,28 +16,26 @@ function App() {
   //   setModal(false);
   // }
 
-  return (
-    <div>
-      {/* //This is the implimentattion of sticky with related to some div ..sticky till some div is present .. */}
-      {
-        /* <button
-        className="btn btn-success mx-auto btn-md btn-dark mx-auto d-block m-5"
-        onClick={setModalFunction}
-      >
-        Click here for Modal
-      </button>
-      {modalState && <Modal closeFunction={resetModal}></Modal>}
-     
-      <Sticky></Sticky> */
-        // here we have Sticky component which sticks to the display relatively .If you want that that parent will have position relative and
-        // child will have position sticky...see example for better understanding ...
-        // also we have modal that works with react state logic .. position fixed and top left right properties .
-        //modal app that works just with js is also present in public folder
-      }
-      {/* <ModalBs></ModalBs>   this component is based on fully bootstrap and no react logic...replace class with className and just import from 
-      bootstrap docs page and make a component out of it ... but it does not use react logic it just use js for implimentation*/}
-    </div>
-  );
+  // {/* //This is the implimentattion of sticky with related to some div ..sticky till some div is present .. */
+  //=========>>>>>>>>>>>put this in return between divs to get result
+  //   /* <button
+  //   className="btn btn-success mx-auto btn-md btn-dark mx-auto d-block m-5"
+  //   onClick={setModalFunction}
+  // >
+  //   Click here for Modal
+  // </button>
+  // {modalState && <Modal closeFunction={resetModal}></Modal>}
+
+  // <Sticky></Sticky> */
+  //   // here we have Sticky component which sticks to the display relatively .If you want that that parent will have position relative and
+  //   // child will have position sticky...see example for better understanding ...
+  //   // also we have modal that works with react state logic .. position fixed and top left right properties .
+  //   //modal app that works just with js is also present in public folder
+
+  // /* <ModalBs></ModalBs>   this component is based on fully bootstrap and no react logic...replace class with className and just import from
+  // bootstrap docs page and make a component out of it ... but it does not use react logic it just use js for implimentation*/}
+
+  return <div></div>;
 }
 
 export default App;
